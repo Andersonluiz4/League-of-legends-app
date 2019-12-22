ac78c3cc981672f269963bf27a91f508d0158135
Andersonluiz4 <andersonluiz4@outlook.com>
2019-12-22 10:34:18 -0300

    Add free week rotation

--- src/app/functions/api.js
+++ src/app/functions/api.js
@@ -1,3 +1,5 @@
+import { stringify } from 'querystring';
+
 let freeWeek;
 let loadImages;
 export function myMethod() {
@@ -5,7 +7,7 @@ export function myMethod() {
 
     axios.all([
         window.axios.get('http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json'),
-        window.axios.get('https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-a84beb3a-e548-4667-9bd4-9a40b8ce417f'),
+        window.axios.get('https://br1.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=RGAPI-a358fbaf-6ffb-4ad2-b208-09a08708b3a4'),
     ])
     .then(function (response) {
         let IdsImage = [];
@@ -21,35 +23,49 @@ export function myMethod() {
 
         for(var x = 0; x < keys.length; x++) {
             if (idsList.includes(parseInt(keys[x].key))) {
-                var championInfo = {'name': keys[x].name, 'title': keys[x].title, 'info': keys[x].info }
+                var freeWeekLoadImage = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + keys[x].name + '_0.jpg'
+                var championInfo = {'name': keys[x].name, 'title': keys[x].title, 'info': keys[x].info, 'url': freeWeekLoadImage}
                 championInfoList.push(championInfo)
                 IdsImage.push(keys[x].id)
-        }
-    }   
+            }
+        }   
         for(var f in IdsImage) {
             var freeWeekUrl = 'http://ddragon.leagueoflegends.com/cdn/9.24.2/img/champion/' + IdsImage[f] + '.png'
-            var freeWeekLoadImage = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + IdsImage[f] + '_0.jpg'
-            var imageInfo = {'name': IdsImage[f], 'url': freeWeekUrl}
-            var loadImageInfo = {'name': IdsImage[f], 'url': freeWeekLoadImage}
             document.getElementById('list').innerHTML += '<img src="' + freeWeekUrl + '" + height=40px;' +'>'
-            loadImages.push(loadImageInfo)
-        } 
-
-
-        function fader(message) {
-            $("#sasuke").fadeOut(100, function() {
-            $(this).html(message).fadeIn(900);
-            
-            });
         }
+
         var thisId=0;
         window.setInterval(function(){
-            $('#img1').attr('src', loadImages[thisId].url);
-            fader(loadImages[thisId].name);
-            thisId++; //increment data array id
-            if (thisId==loadImages.length) thisId=0;
-        },2000);
+            $('#img1').attr('src', championInfoList[thisId].url);
+            faderImage(championInfoList[thisId].url);
+            faderTitle(championInfoList[thisId].title);
+            faderInfo(JSON.stringify(championInfoList[thisId].info));
+            thisId++;
+            if (thisId==championInfoList.length) thisId=0;
+        },3000 );
 
+
+
+
+function faderImage(message) {
+    $("#img2").fadeOut(100, function() {
+    $(this).attr('src', message).fadeIn(900);
+    
+    });
+}
+function faderTitle(message) {
+    $("#champ-title").fadeOut(100, function() {
+    $(this).html(message).fadeIn(900);
+    
+    });
+}
+
+function faderInfo(message) {
+    $("#info-list").fadeOut(100, function() {
+    $(this).html(message).fadeIn(900);
+    
+    });
+}
     });
 }
 
