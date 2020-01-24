d61513955f3e0a5e4d8f09f0cee351a7678e0c65
Andersonluiz4 <andersonluiz4@outlook.com>
2020-01-24 10:44:19 -0300

    Add functional chat

--- src/app/allchampions/allchampions.component.ts
+++ src/app/allchampions/allchampions.component.ts
@@ -10,31 +10,57 @@ import * as $ from 'jquery';
   templateUrl: './allchampions.component.html',
   styleUrls: ['./allchampions.component.css']
 })
-
 export class AllchampionsComponent implements OnInit {
   
   
   summonerName: string;
   click: number = 0;
+  removeFilter: number = 0;
+  idList: any = [];
+
+
   public modelChange(str: string): void {
-        $(".img-thumbnail").each(function(){
+    if(this.click == 0) {
+      $(".champions-images").each(function(){
+        if ($(this).attr('id').search(new RegExp(str, "i")) < 0) {
+            $(this).fadeOut(20);
+        } else {
+            $(this).show();
+        }
+    });
+    }
+    else {
+          $(".filteredIds").each(function(){
           if ($(this).attr('id').search(new RegExp(str, "i")) < 0) {
-              $(this).fadeOut(20);
-          } else {
-              $(this).show();
-          }
-      });
+            $(this).fadeOut(20);
+        } else {
+            $(this).show();
+        }
+      });  
+    }
+  
   }
   public filterByTitle(description) {
-      $(".img-thumbnail").each(function(){
-          if ($(this).attr('title').search(new RegExp(description , "i")) < 0) {
-            $(this).fadeOut(20);
-        }
-        else {
-          $(this).show();
-        }
-      })
-      $(".img-thumbnail").show();
+      $(".filteredIds").attr('class', 'champions-images');
+      $(".champions-images").each(function(){
+        if ($(this).attr('title').search(new RegExp(description , "i")) < 0) {
+          $(this).fadeOut(20);
+      }
+      else {
+        $(this).show();
+        console.log("change to filter")
+        $(this).attr('class', 'filteredIds');
+      }
+      
+    })
+    this.click = 1;
+
+  }
+
+  public removeFilters() {
+    $(".champions-images").show();
+    $(".filteredIds").attr('class', 'champions-images');
+    this.click = 0;
   }
 
   constructor() {
