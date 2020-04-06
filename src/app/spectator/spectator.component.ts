158adbbc3806a89b5bd694c067f01d84b6719245
unknown <andersonluiz4@outlook.com>
2020-04-06 13:04:43 -0300

    Add spectate mode

--- src/app/spectator/spectator.component.ts
+++ src/app/spectator/spectator.component.ts
@@ -14,16 +14,18 @@ declare var require: any
 })
 export class SpectatorComponent implements OnInit {
 
+  finalList = [];
   spectateInfo: any = {};
   userInfo: any = {};
   summonerId = false;
   championsList: any = [];
   summonerName: string;
+  result: any = {};
+  spec: any = {};
 
   constructor(private http:HttpClient) { }
 
   getSpectateInfo(input){
-    console.log(this.summonerName)
     const apiUrl = 'https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + this.summonerName + '?api_key=' + config.apikey
     const promise = this.http.get(apiUrl).toPromise();
     promise.then((data)=>{
@@ -31,11 +33,39 @@ export class SpectatorComponent implements OnInit {
       
       this.http
         .get<any>('https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/' + this.userInfo.id + '?api_key=' + config.apikey)
-        .subscribe(masteryInfo => {
+        .subscribe(async masteryInfo => {
           this.spectateInfo = masteryInfo
-          console.log(spec.getSpectateImages(this.spectateInfo.participants))
-          // styleLoader.loader("#loader", "#summonerInfo", 200, 1000),
-        })
+          this.http
+          .get<any>("http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json")
+          .subscribe(async summonersInfo => {
+            this.spec = summonersInfo
+            const keys = Object.values(this.spec.data)
+            for(var id in this.spectateInfo.participants) {
+                var championInfo = {'name': this.spectateInfo.participants[id].summonerName, 'champ': this.spectateInfo.participants[id].championId, 'teamId': this.spectateInfo.participants[id].teamId}
+                var idsInfo = [championInfo.champ]
+                for(var data = 0; data < keys.length; data++) {
+                    if (idsInfo.includes(parseInt(keys[data].key))) {
+                        var freeWeekLoadImage = '/assets/championImages/loading/' + keys[data].name + '_0.jpg'
+                        console.log(championInfo)
+                        if(championInfo.teamId == 100) {
+                          document.getElementById('list1').innerHTML += '<div><img id="champImg" src="' + "../.." + freeWeekLoadImage + '" + height=300px;' +'> <div id="summonerId">' + championInfo.name + '</div></div>'
+                          $("#list1").css("color", "darksalmon")
+                          $("#list1").css("textAlign", "center")
+                        }
+                        else {
+                          document.getElementById('list2').innerHTML += '<div><img id="champImg" src="' + "../.." + freeWeekLoadImage + '" + height=300px;' + '> <div id="summonerId">' + championInfo.name + '</div></div>'
+                          $("#list2").css("color", "darksalmon")
+                          $("#list2").css("textAlign", "center")
+                          
+                        }
+                      $("#summonerId").css("borderStyle", "groove")
+                      $("#champImg").css("borderStyle", "groove")
+                    }
+                }
+            }
+            // styleLoader.loader("#loader", "#summonerInfo", 200, 1000),
+          })
+      })
     }).catch(()=>{
       styleLoader.loader("#loader2", "#error-message", 500, 1000)
       styleLoader.errorMessage("Invalid summoner name")
--- src/assets/js/functions/spectate/spectateData.js
+++ src/assets/js/functions/spectate/spectateData.js
@@ -2,19 +2,19 @@ let config = require('../../../json/eloAttributes.json')
 window.axios = require('axios');
 
 var finalList = [];
-export function getSpectateImages(playersIds) {
-    
+export async function getSpectateImages(playersIds) {
     axios.all([
         window.axios.get('http://ddragon.leagueoflegends.com/cdn/9.3.1/data/en_US/champion.json')
     ])
-    .then(function (response) {
+    .then(async function (response) {
+        console.log("ala")
         const keys = Object.values(response[0].data.data)
         for(var id in playersIds) {
             var championInfo = {'name': playersIds[id].summonerName, 'champ': playersIds[id].championId, 'teamId': playersIds[id].teamId}
             var idsInfo = [championInfo.champ]
             for(var data = 0; data < keys.length; data++) {
                 if (idsInfo.includes(parseInt(keys[data].key))) {
-                    var freeWeekLoadImage = '/assets/loading/' + keys[data].name + '_0.jpg'
+                    var freeWeekLoadImage = '/assets/championImages/loading/' + keys[data].name + '_0.jpg'
                     var finalDict = {'name': championInfo.name, 'champ': championInfo.champ, 'teamId': championInfo.teamId, 'url': freeWeekLoadImage}
                     finalList.push(finalDict)
                 }
@@ -23,3 +23,4 @@ export function getSpectateImages(playersIds) {
         return finalList
     })
 }
+NpfdcK4uWAU1CHgukCCHJ3Ts_BeFgktdtWK0BbkWA_CMGOs
